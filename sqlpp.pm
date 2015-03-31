79036230dc87e23d3fa3a7a89d8e03d5f4cf9d6f
Egor Starostin <egorst@gmail.com>
2015-03-31 23:42:33 +0300

    a couple of commands in docs; a couple of lines in sqlpp.pm

--- sqlpp.pm
+++ sqlpp.pm
@@ -5,13 +5,28 @@ use lib $ENV{RLWRAP_FILTERDIR};
 use RlwrapFilter;
 use Term::ANSIColor;
 
+our $transposeOutput = 0;
+our $compactOutput   = 0;
+
 sub handle_output {
     s/^(ORA\-\d+:.*)$/\e[31m$1\e[0m/mg;
     return $_;
 }
 
+sub handle_input {
+    $_;
+}
+
+sub handle_echo {
+    $_;
+}
+
 our $ft = new RlwrapFilter;
 
 $ft->output_handler(\&handle_output);
+
+$ft->input_handler(\&handle_input);
+
+$ft->echo_handler(\&handle_echo);
 
 $ft->run;
